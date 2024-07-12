5d9ce6b185edb0a7ded9436b69e15d97aa623b79
domshi <90562421+domshi@users.noreply.github.com>
2024-07-12 23:41:18 +0000

    update

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ AllowedVehiclesList = ['Ford F-150','Chevrolet Silverado','Tesla Cybertruck','To
 
 while (True):
   print("********************************")
-  print("AutoCountry Vehicle Finder v0.1")
+  print("AutoCountry Vehicle Finder v0.3")
   print("********************************")
   print("Please Enter one of the following numbers below from the following menu:")
   print(" ")
   print('1. PRINT all Authorized Vehicles')
   print('2. SEARCH for Authorized Vehicle')
-  print('3. Exit')
+  print('3. ADD Authorized Vehicle')
+  print('4. Exit')
   selection = input("Please enter your choice: ")
   if int(selection) == 1:
     print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
@@ -24,10 +25,18 @@ while (True):
     if vehicleChoice not in AllowedVehiclesList:
       print((vehicleChoice) + ' is not an authorized vehicle, if you received this in error please check the spelling and try again')
     continue
-  if int(selection) != 3:
+  if int(selection) == 3:
+    vehicleChoice = input('Please Enter the full Vehicle name you would like to add: ')
+    if vehicleChoice in AllowedVehiclesList:
+      print((vehicleChoice) + ' is already an authorized vehicle')
+    if vehicleChoice not in AllowedVehiclesList:
+      AllowedVehiclesList.append(vehicleChoice)
+      print('You have added ' + (vehicleChoice) + ' to the authorized vehicle list')
+    continue
+  if int(selection) != 4:
     print("Invalid Choice,Please select a valid choice from the menu")
     continue
-  if int(selection) == 3:
+  if int(selection) == 4:
     print('Thank you for using the AutoCountry Vehicle Finder, good-bye!')
     break
 
